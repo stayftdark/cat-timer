591a1038b5a6bd5cc01781daed5538844c4bc618
stayftdark <stayftdark@gmail.com>
2023-11-09 15:01:41 -0800

    before final

--- script.js
+++ script.js
@@ -4,72 +4,73 @@ document.addEventListener("DOMContentLoaded", function() {
     let isRunning = false;
     let intervals = [60 * 60, 15 * 60]; // Initial intervals in seconds (60 minutes, 15 minutes)
     let currentIntervalIndex = 0;
-  
+
+    // Set initial image source
+    document.getElementById('sailor-cat').src = './images/grey.png';
+
     // Display initial time
     updateTimeDisplay();
-  
+
     // Start button click event
     document.getElementById('start').addEventListener('click', function() {
-        
-      if (!isRunning) {
-        startTimer();
-      }
+        if (!isRunning) {
+            startTimer();
+        }
     });
-  
+
     // Pause button click event
     document.getElementById('pause').addEventListener('click', function() {
-      if (isRunning) {
-        pauseTimer();
-      }
+        if (isRunning) {
+            pauseTimer();
+        }
     });
-  
+
     // Function to start the timer
     function startTimer() {
-      isRunning = true;
-      timer = setInterval(function() {
-        document.getElementById('status').innerText = 'Start!';
-        if (intervals[currentIntervalIndex] > 0) {
-          intervals[currentIntervalIndex]--;
-          updateTimeDisplay();
-        } else {
-          clearInterval(timer);
-          isRunning = false;
-          switchInterval();
-          changeImage(); // Call the function to change the image
-          startTimer();
-          document.getElementById('sailor-cat').src = './images/kiki.png'; // Change the image source 
-
-        }
-      }, 1000); // Update every second
+        isRunning = true;
+        timer = setInterval(function() {
+            document.getElementById('status').innerText = 'Start!';
+            if (intervals[currentIntervalIndex] > 0) {
+                intervals[currentIntervalIndex]--;
+                updateTimeDisplay();
+            } else {
+                clearInterval(timer);
+                isRunning = false;
+                switchInterval();
+                changeImage(); // Call the function to change the image
+                startTimer();
+            }
+        }, 1000); // Update every second
     }
-  
+
     // Function to pause the timer
     function pauseTimer() {
-      isRunning = false;
-      clearInterval(timer);
-      document.getElementById('sailor-cat').src = './images/sailor.png'; // Change the image source 
-      document.getElementById('status').innerText = '...';
+        isRunning = false;
+        clearInterval(timer);
+        document.getElementById('sailor-cat').src = './images/sailor.png'; // Change the image source 
+        document.getElementById('status').innerText = '...';
     }
-  
+
     // Function to update the time display
     function updateTimeDisplay() {
-      const minutes = Math.floor(intervals[currentIntervalIndex] / 60);
-      const seconds = intervals[currentIntervalIndex] % 60;
-      const formattedTime = `${padZero(minutes)}:${padZero(seconds)}`;
-      document.getElementById('timer').innerText = formattedTime;
-      document.getElementById('sailor-cat').src = './images/grey.png'; // Change the image source 
+        const minutes = Math.floor(intervals[currentIntervalIndex] / 60);
+        const seconds = intervals[currentIntervalIndex] % 60;
+        const formattedTime = `${padZero(minutes)}:${padZero(seconds)}`;
+        document.getElementById('timer').innerText = formattedTime;
+
+        // Change the image source only if the timer is running
+        if (isRunning) {
+            document.getElementById('sailor-cat').src = './images/kiki.png';
+        }
     }
-  
+
     // Function to switch to the next interval
     function switchInterval() {
-      currentIntervalIndex = (currentIntervalIndex + 1) % intervals.length;
+        currentIntervalIndex = (currentIntervalIndex + 1) % intervals.length;
     }
-  
+
     // Function to pad zero for single-digit numbers
     function padZero(num) {
-      return num < 10 ? `0${num}` : num;
+        return num < 10 ? `0${num}` : num;
     }
-  
-    
-  });
-  
+});
